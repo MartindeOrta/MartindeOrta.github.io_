f65e4f3d8baff31d5501186e49674e82f3da6547
MartindeOrta <martindeorta@gmail.com>
2023-02-06 11:53:18 -0300

    pruebas

---
+++ app.js
@@ -0,0 +1,182 @@
+minimoporcentaje30= 0
+minimoporcentaje120=0
+var Cantidadmonedascargadasentexto = 10
+
+
+porcentajeMaximo=100
+let datosAth=[{"MONEDA":"IMX","ATH":"1.63"},{"MONEDA":"IOTX","ATH":"0.263"},{"MONEDA":"ROSE","ATH":"0.59443"},{"MONEDA":"PEOPLE","ATH":"0.17768"},{"MONEDA":"COTI","ATH":"0.70408"},{"MONEDA":"KNC","ATH":"4.31886"},{"MONEDA":"GTC","ATH":"28.99"},{"MONEDA":"KAVA","ATH":"9.226"},{"MONEDA":"ETC","ATH":"179"},{"MONEDA":"ICP","ATH":"478.87"},{"MONEDA":"CHR","ATH":"1.5086"},{"MONEDA":"ADA","ATH":"3.1052"},{"MONEDA":"ANT","ATH":"14.25"},{"MONEDA":"DUSK","ATH":"1.07711"},{"MONEDA":"NEAR","ATH":"20.6424"},{"MONEDA":"NEO","ATH":"141.379"},{"MONEDA":"CELR","ATH":"0.1985"},{"MONEDA":"ONE","ATH":"0.381"},{"MONEDA":"CELO","ATH":"7.88"},{"MONEDA":"HBAR","ATH":"0.57644"},{"MONEDA":"RUNE","ATH":"21.34"},{"MONEDA":"GALA","ATH":"0.85"},{"MONEDA":"OGN","ATH":"3.4"},{"MONEDA":"RSR","ATH":"0.119"},{"MONEDA":"NKN","ATH":"1.484"},{"MONEDA":"MASK","ATH":"22.5739"},{"MONEDA":"QTUM","ATH":"35.69"},{"MONEDA":"AAVE","ATH":"666.8"},{"MONEDA":"DYDX","ATH":"27.875"},{"MONEDA":"KLAY","ATH":"1.88"},{"MONEDA":"CVC","ATH":"0.98"},{"MONEDA":"DENT","ATH":"0.022747"},{"MONEDA":"ATOM","ATH":"44.789"},{"MONEDA":"HNT","ATH":"60.27"},{"MONEDA":"OMG","ATH":"20.1726"},{"MONEDA":"ANKR","ATH":"0.216781"},{"MONEDA":"LUNA","ATH":"103.61"},{"MONEDA":"AR","ATH":"91.27"},{"MONEDA":"GRT","ATH":"2.86547"},{"MONEDA":"XTZ","ATH":"9.177"},{"MONEDA":"LPT","ATH":"75.785"},{"MONEDA":"AKRO","ATH":"0.08878"},{"MONEDA":"COMP","ATH":"913.02"},{"MONEDA":"TRB","ATH":"164.69"},{"MONEDA":"THETA","ATH":"15.9245"},{"MONEDA":"STORJ","ATH":"3.835"},{"MONEDA":"LIT","ATH":"13.444"},{"MONEDA":"DODO","ATH":"8.94"},{"MONEDA":"FTM","ATH":"3.4908"},{"MONEDA":"LINK","ATH":"53.07"},{"MONEDA":"ALGO","ATH":"2.8717"},{"MONEDA":"ZEN","ATH":"170"},{"MONEDA":"STMX","ATH":"0.09981"},{"MONEDA":"ZRX","ATH":"2.41"},{"MONEDA":"VET","ATH":"0.279824"},{"MONEDA":"RVN","ATH":"0.27369"},{"MONEDA":"CHZ","ATH":"0.958"},{"MONEDA":"UNI","ATH":"45.0876"},{"MONEDA":"CRV","ATH":"6.809"},{"MONEDA":"AUDIO","ATH":"3.9667"},{"MONEDA":"RAY","ATH":"16.97"},{"MONEDA":"SC","ATH":"0.064289"},{"MONEDA":"SAND","ATH":"8.5"},{"MONEDA":"C98","ATH":"6.4588"},{"MONEDA":"LINA","ATH":"0.20453"},{"MONEDA":"MANA","ATH":"5.917"},{"MONEDA":"BAT","ATH":"1.93"},{"MONEDA":"MATIC","ATH":"2.925"},{"MONEDA":"LRC","ATH":"3.8276"},{"MONEDA":"XMR","ATH":"521"},{"MONEDA":"ENS","ATH":"78.342"},{"MONEDA":"BAKE","ATH":"5.9073"},{"MONEDA":"MTL","ATH":"7.2106"},{"MONEDA":"ZEC","ATH":"372.62"},{"MONEDA":"BLZ","ATH":"0.66932"},{"MONEDA":"AVAX","ATH":"148"},{"MONEDA":"BTT","ATH":"0.01096"},{"MONEDA":"HOT","ATH":"0.0319"},{"MONEDA":"BCH","ATH":"1642.62"},{"MONEDA":"EGLD","ATH":"544.95"},{"MONEDA":"BEL","ATH":"5.86197"},{"MONEDA":"SRM","ATH":"13.75"},{"MONEDA":"XLM","ATH":"0.79943"},{"MONEDA":"MKR","ATH":"6344.39"},{"MONEDA":"KSM","ATH":"626.68"},{"MONEDA":"BNB","ATH":"692.9"},{"MONEDA":"EOS","ATH":"14.927"},{"MONEDA":"ONT","ATH":"2.9672"},{"MONEDA":"CTSI","ATH":"1.7295"},{"MONEDA":"SOL","ATH":"260.064"},{"MONEDA":"SXP","ATH":"5.8774"},{"MONEDA":"DOGE","ATH":"0.744178"},{"MONEDA":"REN","ATH":"1.8495"},{"MONEDA":"LTC","ATH":"413.94"},{"MONEDA":"DEFI","ATH":"3847.3"},{"MONEDA":"SUSHI","ATH":"23.4732"},{"MONEDA":"OCEAN","ATH":"1.94873"},{"MONEDA":"NU","ATH":"1.2726"},{"MONEDA":"DOT","ATH":"55.2"},{"MONEDA":"ALPHA","ATH":"2.98"},{"MONEDA":"SNX","ATH":"29.294"},{"MONEDA":"FLM","ATH":"1.25"},{"MONEDA":"ICX","ATH":"3.2055"},{"MONEDA":"BAL","ATH":"75.408"},{"MONEDA":"YFII","ATH":"7829.6"},{"MONEDA":"IOST","ATH":"0.091241"},{"MONEDA":"TRX","ATH":"0.18077"},{"MONEDA":"ENJ","ATH":"4.8547"},{"MONEDA":"ZIL","ATH":"0.25762"},{"MONEDA":"CTK","ATH":"3.97966"},{"MONEDA":"1INCH","ATH":"7.889"},{"MONEDA":"DASH","ATH":"478.49"},{"MONEDA":"ETH","ATH":"4877.54"},{"MONEDA":"SKL","ATH":"1.25214"},{"MONEDA":"XRP","ATH":"1.977"},{"MONEDA":"BTS","ATH":"0.175"},{"MONEDA":"ALICE","ATH":"28.71"},{"MONEDA":"1000SHIB","ATH":"0.089575"},{"MONEDA":"BTCDOM","ATH":"1272.5"},{"MONEDA":"ARPA","ATH":"0.27638"},{"MONEDA":"XEM","ATH":"0.8439"},{"MONEDA":"IOTA","ATH":"2.6864"},{"MONEDA":"REEF","ATH":"0.059075"},{"MONEDA":"KEEP","ATH":"1.2"},{"MONEDA":"RLC","ATH":"16.4"},{"MONEDA":"SFP","ATH":"3.983"},{"MONEDA":"BAND","ATH":"23.3555"},{"MONEDA":"FIL","ATH":"238.511"},{"MONEDA":"TOMO","ATH":"3.9318"},{"MONEDA":"YFI","ATH":"95217.1"},{"MONEDA":"DGB","ATH":"0.18264"},{"MONEDA":"UNFI","ATH":"44.155"},{"MONEDA":"1000XEC","ATH":"0.275"},{"MONEDA":"BTC","ATH":"69198.7"},{"MONEDA":"ATA","ATH":"1.74"},{"MONEDA":"TLM","ATH":"0.5715"},{"MONEDA":"WAVES","ATH":"42"},{"MONEDA":"AXS","ATH":"165.88"}]
+
+//
+ // 9 es igua a 10 moneadas porque comienza desde 0
+var    monedasTelegramArray= [[]]
+ var monedasTelegramArray120=[[]]
+
+var    canvasTelegramArray= [[]]
+var    canvasTelegramArray120= [[]]
+var monedasTelegramJson;
+
+var monedas120minutosJson;
+
+var arrayjson;
+
+var arrayjson120;
+let endpoint = ' https://fapi.binance.com/fapi/v1/ticker/24hr'
+let ColumnaPorcentaje;
+let arrayGraficoMonedas = []
+
+let arrayGrafico120 = [[]]
+Cuentagrafico = 0;
+let a;
+let body = ''
+let MonedaTelegram
+
+var conservarsimbolo = []
+var conservarsimbolo120 = []
+
+let horaPrueva= 0 
+moneda="BTCUSDT"
+let velas = 30
+
+function drawChart(data,cuenta,porcentaje,precio) 
+{
+  Cuentagrafico +=1;
+  var data = google.visualization.arrayToDataTable(data, true); 
+  var options = 
+  { 
+    legend:'none', 
+    //chartArea:{left:100,top:100,width:'100%',height:'75%'},
+    title: cuenta +", $"+precio+", "+porcentaje+"%", 
+    bar: { groupWidth: '92%' },  
+    candlestick: 
+    { 
+      fallingColor: { strokeWidth: 0, fill: 'red' }, // red 
+      risingColor: { strokeWidth: 0, fill: '#0f9d58',stroke:'black' } // stroke:'black' color borde 
+      // fallingColor: {fill: 'blue' }  //funciona color de las velas que caen risingColor es para las velas que suben 
+      //fallingColor: {stroke: 'red' } 
+      // green 
+    } 
+  }; 
+  var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'+(cuenta))); 
+  chart.draw(data, options); 
+} 
+
+let link= 'https://fapi.binance.com/fapi/v1/klines?symbol='+ moneda +'&limit='+velas+'&interval=1m' 
+// original let ws = new WebSocket('wss://fstream.binance.com/ws/btcusdt@trade'); 
+let ws = new WebSocket('wss://fstream.binance.com/ws/'+ moneda.toLowerCase() +'@kline_1m'); 
+let wstiket = new WebSocket('wss://fstream.binance.com/ws/!miniTicker@arr'); 
+fetch(endpoint)
+.then( respuesta => respuesta.json() )
+.then( datos =>{ 
+
+ mostrarData(datos)
+
+ //setInterval(function(){  
+  //location.reload();
+// },10000)
+
+
+ })
+
+
+ // anterior julio14: .catch( e => location.reload())
+
+
+   
+  
+const mostrarData = (data)=>
+{
+  let contador= 0;
+  for (let u=0; u <  data.length; u++) 
+  {
+    let usdt =data[u].symbol.substr(-4,4)
+    let volumen = data[u].quoteVolume
+    if ( usdt== "USDT" && volumen > 50000000 ) // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvol
+    { 
+        contador += 1
+        monedaSymbol = data[u].symbol
+        let absPriceChange = Math.abs(data[u].priceChangePercent)
+        absPriceChange = absPriceChange.toFixed(1)
+        arrayGraficoMonedas[`${data[u].symbol}`]={"arraygrafico":[[]],"porcentaje":0,"precio":0}
+        let link= 'https://fapi.binance.com/fapi/v1/klines?symbol='+ data[u].symbol +'&limit=30&interval=1m'
+        fetch(link)
+        .then(response => response.json())
+        .then(data1 => 
+        {  
+          var high =[]
+          var low = []
+          var price = 0
+          var max = 0
+          var min = 0
+          for (i = 0 ; i < data1.length; i++) 
+          { 
+            arrayGraficoMonedas[`${data[u].symbol}`].arraygrafico[i]=[i+1,parseFloat( data1[i][3]),parseFloat(data1[i][1]),parseFloat(data1[i][4]),parseFloat(data1[i][2])] 
+            high[i]=parseFloat(arrayGraficoMonedas[`${data[u].symbol}`].arraygrafico[i][4])
+            low[i]=parseFloat(arrayGraficoMonedas[`${data[u].symbol}`].arraygrafico[i][1])
+          } 
+          cierreAnterior=data1[data1.length-1][6]
+          porcentaje = 0
+        
+          body= document.getElementById("body").innerHTML
+          body+=`<tr><td id="chart_div${data[u].symbol}" style="width: 900px; height: 500px;"></td></tr>`
+          document.getElementById("body").innerHTML=body
+          Cuentagrafico +=1;
+          PRUEVA=Cuentagrafico
+          let ws = new WebSocket('wss://fstream.binance.com/ws/'+data[u].symbol.toLowerCase()+'@kline_1m'); 
+          ws.onmessage = (event) => 
+          { 
+            let stockObject = JSON.parse(event.data); 
+            price = parseFloat( stockObject.k.c)
+            document.getElementById("timeClose").innerText="hora cierre: " + (stockObject.k.T/1000).toFixed()
+            document.getElementById("timeOpen").innerText="hora apertura: " + (stockObject.k.t/1000).toFixed()
+            document.getElementById("time").innerText="hora actual: " + (stockObject.E/1000).toFixed()
+            if (cierreAnterior !=stockObject.k.T)
+            {
+              cierreAnterior= stockObject.k.T
+              arrayGraficoMonedas[`${stockObject.s}`].arraygrafico.push([velas,parseFloat(stockObject.k.l),parseFloat(stockObject.k.o),parseFloat(stockObject.k.c),parseFloat(stockObject.k.h)] )
+              arrayGraficoMonedas[`${stockObject.s}`].arraygrafico.shift()
+              for(e = 0 ; e< arrayGraficoMonedas[`${stockObject.s}`].arraygrafico.length; e++)
+              {
+                high[e]=parseFloat( arrayGraficoMonedas[`${stockObject.s}`].arraygrafico[e][4])
+                low[e]=parseFloat(arrayGraficoMonedas[`${stockObject.s}`].arraygrafico[e][1])
+                arrayGraficoMonedas[`${stockObject.s}`].arraygrafico[e][0]=e+1
+              }
+            }
+            max = high.reduce(function(a, b) 
+            {
+              return Math.max(a, b);
+            }, -Infinity);
+            min = high.reduce(function(a, b) 
+            {
+              return Math.min(a, b);
+            }, Infinity);
+            porcentaje =  porcentajeVariacion(max,min,price).toFixed(2)
+            arrayGraficoMonedas[`${stockObject.s}`].arraygrafico[velas-1]=[velas,parseFloat(stockObject.k.l),parseFloat(stockObject.k.o),parseFloat(stockObject.k.c),parseFloat(stockObject.k.h)] 
+            arrayGraficoMonedas[`${stockObject.s}`].porcentaje=porcentaje
+            arrayGraficoMonedas[`${stockObject.s}`].precio=price
+            google.charts.load('current', {'packages':['corechart']}); 
+            google.charts.setOnLoadCallback(drawChart(arrayGraficoMonedas[`${stockObject.s}`].arraygrafico,stockObject.s, arrayGraficoMonedas[`${stockObject.s}`].porcentaje,price)); 
+          }
+        })
+    }
+  }
+  
+
+}
+function porcentajeVariacion(preciomasalto1,preciomasbajo1,ultimoprecio1)
+{    
+  let porcentajeac;                     
+   porcentajetotal = 100 - (preciomasbajo1 * 100 / preciomasalto1)
+   if ( (preciomasalto1 - ultimoprecio1) >= (ultimoprecio1 - preciomasbajo1))
+   {
+      porcentajeac = 100 - (ultimoprecio1 * 100 / preciomasalto1)
+   }
+   else 
+   {
+      porcentajeac = 100 - (ultimoprecio1 * 100 / preciomasbajo1)
+   }
+   porcentajeac =    Math.abs(porcentajeac)
+   return porcentajeac 
+}
+   
+   
+  
